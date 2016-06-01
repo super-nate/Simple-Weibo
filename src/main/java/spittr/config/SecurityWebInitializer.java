78c68081f7be42277a0a0bd8dccc2565b8e4e575
supermanheng21 <supermamheng21@gmail.com>
2016-06-01 20:12:10 +0300

    comment the security related code

--- src/main/java/spittr/config/SecurityWebInitializer.java
+++ src/main/java/spittr/config/SecurityWebInitializer.java
@@ -1,6 +1,6 @@
-package spittr.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
-}
+//package spittr.config;
+//
+//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+//
+//public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
+//}
