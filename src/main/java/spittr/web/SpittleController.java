48971c4b61db579a6c46cb5cb4aba84716001a43
supermanheng21 <supermamheng21@gmail.com>
2016-07-04 16:29:05 +0300

    SPITTR-35: finish the query para and simple paging

--- src/main/java/spittr/web/SpittleController.java
+++ src/main/java/spittr/web/SpittleController.java
@@ -4,12 +4,14 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.support.PagedListHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import spittr.entity.Spittle;
 import spittr.entity.SpittleForm;
 import spittr.service.SpittleManager;
@@ -27,9 +29,25 @@ public class SpittleController {
     this.spittleManager = spittleManager;
   }
 
-  @RequestMapping(method=RequestMethod.GET)
+/*  @RequestMapping(method=RequestMethod.GET)
   public List<Spittle> spittles() {
     return spittleManager.findAll();
+  }*/
+
+  @RequestMapping(method=RequestMethod.GET)
+  public String spittles(
+          @RequestParam(value="page",
+                  defaultValue="1") int page, Model model) {
+
+    PagedListHolder<Spittle> pagedListHolder = new PagedListHolder<>(spittleManager.findAll());
+    pagedListHolder.setPageSize(2);
+
+    if(page < 1 || page > pagedListHolder.getPageCount()) page=1;
+    pagedListHolder.setPage(page-1);
+
+    model.addAttribute("page", page);
+    model.addAttribute("spittleList", pagedListHolder.getPageList());
+    return "spittles";
   }
 
   @RequestMapping(value="/{spittleId}", method=RequestMethod.GET)
