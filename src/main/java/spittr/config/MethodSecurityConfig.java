057a6b03447a67d0211920a79078a5f6c079d0ec
supermanheng21 <supermamheng21@gmail.com>
2016-07-15 21:18:28 +0300

    SPITTR-36: add a method secure but need to fix postfilter

--- src/main/java/spittr/config/MethodSecurityConfig.java
+++ src/main/java/spittr/config/MethodSecurityConfig.java
@@ -1,7 +1,24 @@
+/*
 package spittr.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 
-@EnableGlobalMethodSecurity
-public class MethodSecurityConfig {
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+    @Autowired
+    private AuthenticationManager am;
+
+    @Override
+    protected AuthenticationManager authenticationManager() {
+        return am;
+    }
 }
+*/
